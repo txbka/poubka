aae1fe1d5a9653309ebd938e0ff90d7ae61b9a4f
Tomasz Kikuła <tomasz.kikula@gmail.com>
2022-08-04 00:08:16 +0200

    Initial Commit

---
+++ poubka/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  poubka
+//
+//  Created by Tomasz Kikuła on 04/08/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
