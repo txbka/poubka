d6dc0f73d4c8f39fa931fa83d0ab8ad5171610ed
Tomasz Kikuła <tomasz.kikula@gmail.com>
2022-08-05 00:40:54 +0200

    initial commit

--- poubka/AppDelegate.swift
+++ poubka/AppDelegate.swift
@@ -11,10 +11,14 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     
-
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+    var feed = 0
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name("bee-ico"))
+        }
+        constructMenu()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -24,7 +28,22 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
-
-
+    
+    @objc func adobtBee(_ sender: NSMenuItem)
+    {
+        let url = URL(string: "https://adoptujpszczole.pl/")!
+        NSWorkspace.shared.open(url)
+    }
+    
+    func constructMenu()
+    {
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(title: "Adopt Bee Bzzz!", action: #selector(AppDelegate.adobtBee(_:)), keyEquivalent: "a"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit Poubka", action: #selector(NSApplication.terminate(_:)),  keyEquivalent: "q"))
+        
+        statusItem.menu = menu
+    }
 }
 
